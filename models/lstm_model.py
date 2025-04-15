3ab9df3c0b9b15a63944073fbcbd294fe7b4f222
Ahmed-Tohamy <54726171+Ahmed-Tohamy@users.noreply.github.com>
2025-04-15 01:57:46 -0500

    Update lstm_model.py

--- models/lstm_model.py
+++ models/lstm_model.py
@@ -1,1 +1,32 @@
-#lstm_model
+
+import torch
+
+import torch.nn as nn
+
+
+
+class LSTMModel(nn.Module):
+
+    def __init__(self, vocab_size, embed_size=128, hidden_size=256, num_layers=1):
+
+        super(LSTMModel, self).__init__()
+
+        self.embed = nn.Embedding(vocab_size, embed_size)
+
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+
+        self.fc = nn.Linear(hidden_size, vocab_size)
+
+
+
+    def forward(self, x):
+
+        x = self.embed(x)
+
+        output, _ = self.lstm(x)
+
+        logits = self.fc(output)
+
+        return logits
+
+
